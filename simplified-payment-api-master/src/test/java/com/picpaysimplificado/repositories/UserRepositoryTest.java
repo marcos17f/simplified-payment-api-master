cbaf882d8aa454ac360cc130438790c16aaf4141
Marcos Felipe Sousa dos santos <okissidamaph3@gmail.com>
2026-01-06 19:54:34 -0300

    Initial commit

---
+++ simplified-payment-api-master/src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findUserByDocument() {
+    }
+}
